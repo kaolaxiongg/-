5f713fb6522945e17a6c4f8334a6275055a6338f
kaolaxiongg <43628463+kaolaxiongg@users.noreply.github.com>
2018-09-27 11:07:33 +0800

    Add files via upload

---
+++ 猜字游戏.py
@@ -0,0 +1,9 @@
+print("``````测试~~~~~~~")
+temp = input("请输入1到100之间的数字:")
+guess = int(temp)
+if guess >50:
+    print("你怎么知道的")
+    print("哼，猜对了也没有奖励的哦")
+else:
+    print("猜错了，我心里面想的是8哦！")
+print("游戏结束，不和你玩了")
